740eb3e5c866f032489e2d891481a7e0baea5ccf
Zachary <zacharylimjx@gmail.com>
2019-12-25 22:35:03 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+# CNA-Scraper
+[![Build Status](https://travis-ci.com/ZacBytes/cna-scraper.svg?branch=master)](https://travis-ci.com/ZacBytes/st-scraper)
+
+Easy to use node_js scraper for Channel News Asia articles.
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install st-scraper.
+
+```bash
+npm install cna-scraper
+```
+
+## Usage
+
+```js
+const cnascraper = require('cna-scraper');
+
+cnascraper.ScrapeArticle('CNA ARTICLE URL HERE', function(err, ArticleData){
+   console.log(ArticleData) //logs entire article data object
+
+   console.log(ArticleData.title) //logs article title
+   console.log(ArticleData.category) //logs article ctegory
+   console.log(ArticleData.text) //logs article text
+   console.log(ArticleData.postdate) //logs article post date
+   console.log(ArticleData.img) //logs image source (parsed for you)
+   console.log(ArticleData.imgcaption) //logs article image caption
+   console.log(ArticleData.source) //logs article source
+});
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to test before making pull requests.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,52 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const validUrl = require('valid-url');
+const srcset = require('srcset');
+
+function cnascraper () {}
+
+cnascraper.prototype.ScrapeArticle = function (articleURL, callback) {
+	articleURL = articleURL.toLowerCase()
+	if (validUrl.isUri(articleURL) && articleURL.includes('channelnewsasia.com')){
+		//Proceed with scraping
+		request(articleURL, function (error, response, body) {
+		
+			if (error) return console.error(error);
+			const $ = cheerio.load(body);
+
+			var title = $('h1.article_title').text();
+			var category = $('span.article__category').text()
+			var imgparsed = srcset.parse($('picture.picture__container').find('source').attr('data-srcset')); 
+			var img = imgparsed[3];
+
+			var imgcaption = $('figcaption.picture_caption').text();
+			var postdate = $('time.article_details-item').text()
+			var source = $('span.article_source').text();
+
+			var text = "";
+     	 	$("div.c-rte--article").each(function(i, item){
+      			text += ($("p", item).text())
+      		});
+
+			var ArticleData = {};
+			ArticleData.title = title;
+			ArticleData.category = category;
+			ArticleData.text = text;
+			ArticleData.source = source;
+			ArticleData.postdate = postdate;
+			ArticleData.img = img;
+			ArticleData.imgcaption = imgcaption;
+			callback(null, ArticleData)
+
+
+		});
+
+	} 
+	else {
+		callback("Invalid articleURL provided to ScrapeArticle function");
+	}
+}
+
+
+module.exports = new cnascraper()
+  
